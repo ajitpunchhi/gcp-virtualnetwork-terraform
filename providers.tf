d17094a106bf9555f823686e5223e80abaac172a
ajitpunchhi <ajit.1431@gmail.com>
2025-05-02 18:18:46 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,9 +1,19 @@
-#GCP Provider 
+# # This file is used to configure the providers for the Terraform project.
+# # It specifies the required providers and their versions.
+# # It also sets up the Google Cloud provider with the necessary credentials.
+# # The providers are used to create and manage resources in Google Cloud.
+
+
 terraform {
+  required_version = ">= 1.3"
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "5.41.0"
+      source  = "hashicorp/google"
+      version = ">= 4.26, < 7"
+    }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.26, < 7"
     }
   }
-}
